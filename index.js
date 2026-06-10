b7fb3d691ad0e4edc6774c3fd0a8893dd9e2a618
Abel Almaraz <almarazabeljesus@gmail.com>
2026-06-10 20:23:24 -0300

    agregamos la configuracion de los puertos del servidor

---
+++ index.js
@@ -0,0 +1,9 @@
+import Server from "./src/server/config.js";
+
+//instanciar la clase server
+
+const server = new Server()
+//agregar los endpoints
+
+//escuchar el puerto correspondiente
+server.listen();
