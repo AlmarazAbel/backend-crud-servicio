1e00296015f796a76fa629750095cd8fd849eb5e
Abel Almaraz <almarazabeljesus@gmail.com>
2026-06-30 16:12:06 -0300

    agregamos middleware personalizado para validar las propiedades de nombreServicio

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { borrarServicioPorID, buscarServicioPorID, crearServicio, editarServicioPorID, listarServicios, prueba } from "../controllers/servicios.controllers.js";
+import { validacionServicio } from "../middleware/validacionServicio.js";
 
 const router = Router();
 //http://localhost:3000/api/servcicios/test
 
 //get-post-put-patch-delete
 router.route('/test').get(prueba)
-router.route('/').post(crearServicio).get(listarServicios)
+router.route('/').post(validacionServicio,crearServicio).get(listarServicios)
 router.route('/:id').get(buscarServicioPorID).delete(borrarServicioPorID).put(editarServicioPorID).patch(editarServicioPorID)
 
 export default router
