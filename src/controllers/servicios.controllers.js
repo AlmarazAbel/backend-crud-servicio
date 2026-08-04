5553567b2a395ae198e279f9fe3d44fa44d89f8c
Abel Almaraz <almarazabeljesus@gmail.com>
2026-08-04 01:42:57 -0300

    agregamos el populate al controlador de buscarServicioPorId para mostrar el nombre y descripcion de la categoria

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -26,8 +26,11 @@ export const crearServicio = async (req, res) => {
 
 export const listarServicios = async (req, res) => {
   try {
-    const servicios = await Servicio.find().populate('categoria','nombre descripcion');
-    res.status(200).json(servicios)
+    const servicios = await Servicio.find().populate(
+      "categoria",
+      "nombre descripcion",
+    );
+    res.status(200).json(servicios);
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -38,13 +41,17 @@ export const listarServicios = async (req, res) => {
 
 export const buscarServicioPorID = async (req, res) => {
   try {
-   
-    const servicioBuscado = await Servicio.findById(req.params.id);
-    if(!servicioBuscado){
-        return  res.status(404).json({mensaje:'no se encontro un servicio con el id enviado'})
+    const servicioBuscado = await Servicio.findById(req.params.id).populate(
+      "categoria",
+      "nombre descripcion",
+    );
+    
+    if (!servicioBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "no se encontro un servicio con el id enviado" });
     }
-    res.status(200).json(servicioBuscado)
-
+    res.status(200).json(servicioBuscado);
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -54,13 +61,13 @@ export const buscarServicioPorID = async (req, res) => {
 };
 export const borrarServicioPorID = async (req, res) => {
   try {
-    
     const servicioBorrado = await Servicio.findByIdAndDelete(req.params.id);
-    if(!servicioBorrado){
-        return  res.status(404).json({mensaje:'no se encontro un servicio con el id enviado'})
+    if (!servicioBorrado) {
+      return res
+        .status(404)
+        .json({ mensaje: "no se encontro un servicio con el id enviado" });
     }
-    res.status(200).json({mensaje:'El servicio se borro correctamente'})
-
+    res.status(200).json({ mensaje: "El servicio se borro correctamente" });
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -70,13 +77,22 @@ export const borrarServicioPorID = async (req, res) => {
 };
 export const editarServicioPorID = async (req, res) => {
   try {
-    
-    const servicioEditado = await Servicio.findByIdAndUpdate(req.params.id,req.body,{new:true});
-    if(!servicioEditado){
-        return  res.status(404).json({mensaje:'no se encontro un servicio con el id enviado'})
+    const servicioEditado = await Servicio.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    if (!servicioEditado) {
+      return res
+        .status(404)
+        .json({ mensaje: "no se encontro un servicio con el id enviado" });
     }
-    res.status(200).json({mensaje:'El servicio se actualizo  correctamente',servicio:servicioEditado})
-
+    res
+      .status(200)
+      .json({
+        mensaje: "El servicio se actualizo  correctamente",
+        servicio: servicioEditado,
+      });
   } catch (error) {
     console.error(error);
     res.status(500).json({
